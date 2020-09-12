fc3e6c5653b6ed6bc787da0a7cadf1da9d50aff7
Ninefrm <samuelfrm77@gmail.com>
2020-09-12 12:44:33 -0500

    Practica 1

---
+++ Practica-1/P1.pl
@@ -0,0 +1,43 @@
+% clausuas % 
+%relaciones (más de un argumento) 
+amigo(pedro, antonio). 
+amigo(pedro, flora). 
+amigo(pedro, juan). 
+amigo(pedro, vicente). 
+amigo(luis, felipe). 
+amigo(luis, maria). 
+amigo(luis, vicente). 
+amigo(carlos, paloma). 
+amigo(carlos, lucia). 
+amigo(carlos, vicente). 
+amigo(carlos, juan).
+amigo(fernando, eva). 
+amigo(fernando, pedro). 
+
+%propiedad(un argumento) 
+millonario(pedro). 
+millonario(antonio). 
+millonario(flora). 
+millonario(felipe). 
+millonario(lucia). 
+
+mexicano(eva). 
+mexicano(pedro).
+mexicano(luis). 
+mexicano(flora). 
+mexicano(felipe).
+mexicano(lucia). 
+
+soltero(pedro).
+soltero(flora). 
+soltero(eva). 
+soltero(luis). 
+casado(felipe,maria). 
+casado(carlos,lucia). 
+
+hermano(carlos,maria). 
+hermano(pedro,luis). 
+
+padre_de(carlos, fernando). 
+padre_de(antonio, maria). 
+padre_de(antonio, carlos).
