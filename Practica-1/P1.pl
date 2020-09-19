f6dc67b156499d6b5efc798684bd444f51bda56e
Ninefrm <samuelfrm77@gmail.com>
2020-09-19 13:03:18 -0500

    Practica 1 terminada y Practica 2 agregada

--- Practica-1/P1.pl
+++ Practica-1/P1.pl
@@ -41,3 +41,45 @@ hermano(pedro,luis).
 padre_de(carlos, fernando). 
 padre_de(antonio, maria). 
 padre_de(antonio, carlos).
+
+%%rules
+
+padremillonario(X) :-
+    padre_de(Y, X),       
+    millonario(Y).
+
+mexicanomillonariosoltero(X) :-
+    millonario(X),
+    soltero(X),
+    mexicano(X).
+
+mexicanomillonariocasado(X, Y) :-
+    millonario(X),
+    casado(X, Y),
+    mexicano(X).
+
+amigodelamigo(X) :-
+    amigo(Y,X),
+    amigo(Y, Z),
+    soltero(Z),
+    millonario(Z).
+
+millonaria(X) :-
+    casado(?, X),
+    millonario(X).
+
+cunadomillonario(X) :-
+    casado(X, Y),
+    hermano(Y, Z),
+    millonario(Z).    
+
+amigosmillonarios(X):-
+    amigo(X,Y),
+    write(Y),
+    millonario(X),
+    millonario(Y).
+
+nueramillonaria(X) :-
+    padre_de(X, Y),
+    casado(Y, Z),
+    millonario(Z).
